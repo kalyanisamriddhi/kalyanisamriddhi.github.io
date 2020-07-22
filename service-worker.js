c6b399227a8653d0793844d0c43237bb1e9ace39
avisionx <avi17223@iiitd.ac.in>
2020-07-22 19:16:29 +0000

    Deploying to gh-pages from  @ 969c6ea144a1988b6b0ed51632fe93c33acd1080 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.da317e18463110c2ec989505908d14a6.js"
+  "/precache-manifest.36962f8d93a2047e6c4bee2d59431c19.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
