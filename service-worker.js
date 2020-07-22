55be3809ac69425b837ddb26c157c83aeea8981c
avisionx <avi17223@iiitd.ac.in>
2020-07-22 21:29:38 +0000

    Deploying to gh-pages from  @ bf62bbdb56108947c42b10f20c4b37ec19de3e1c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.77df2f66dedcc6254ec76b0d110d94a2.js"
+  "/precache-manifest.b0889b78888075a2445e2e756e995763.js"
 );
 
 self.addEventListener('message', (event) => {
