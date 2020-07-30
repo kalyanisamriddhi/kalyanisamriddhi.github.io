92fb756531468e681354504ca03836a0fca316d5
Yash Kalyani <yash17273@iiitd.ac.in>
2020-07-30 14:29:43 +0530

    Update text.js

--- src/text.js
+++ src/text.js
@@ -8,8 +8,8 @@ export const text = {
   about: {
     title: "About Our Foundation",
     head: "We Are In A Mission To Help The Helpless",
-    text1: "Kalyani Samriddhi is a non-profit charity organization and an integral part of Kalyani Group of Industries.",
-    text2: "We aim to provide donations, not exclusivey monetary, to those in need. Funds for scholarships and medical facilities are provided as well as ration for the poor.",
+    text1: "One act of generosity is all it takes to bring hope to those who need it. Kalyani Samriddhi is a non-profit charity organization and an integral part of Kalyani Group of Industries.",
+    text2: "We aim to provide donations, not exclusivey monetary, to those in need. We take an active interest in the civic, cultural, social and moral welfare of the community.",
     title2: "What We Are Doing",
     head2: "Lorem ipsum dolor sit amet consectetur elit"
   },
@@ -23,7 +23,7 @@ export const aboutArray = [
   {
     icon: mdiBookOpenPageVariant,
     title: "Educational Scholarship",
-    text: "Edcuation is a right to every child, and we aim to not deprive any child of that. By providing educational scholarships, we aim to assist the financially burdened in achieving an educational qualification."
+    text: "Edcuation is a right, and we aim to not deprive any child of that. By providing scholarships, we aim to assist the financially burdened in achieving an educational qualification."
   },
   {
     icon: mdiHospitalBox,
@@ -33,7 +33,7 @@ export const aboutArray = [
   {
     icon: mdiFoodApple,
     title: "Clean Water & Food",
-    text: "We provide basic amenities like clean food and water to the homeless and poor."
+    text: "We serve to ensure basic amenities like clean food and water are available to all community members."
   }
 ]
 
